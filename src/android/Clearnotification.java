988dfd999e2ef89464b3a3e4b10d059043b13072
entom <tomasz@entom.info>
2018-10-26 08:16:30 +0200

    remove notification with selected id value

--- src/android/Clearnotification.java
+++ src/android/Clearnotification.java
@@ -1,5 +1,11 @@
 package info.entom.plugin;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,6 +18,19 @@ public class Clearnotification extends CordovaPlugin {
         if (action.equals("clear")) {
             String id = data.getString(0);
             String message = "Id = " + id;
+            StatusBarNotification[] notifs;
+
+            NotificationManager notificationManager = (NotificationManager)this.cordova.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                notifs = notificationManager.getActiveNotifications();
+                for (StatusBarNotification notif : notifs) {
+                    StatusBarNotification notif1 = notif;
+                    if (Integer.parseInt(id) == notif.getId()) {
+                        notificationManager.cancel(notif.getTag(), notif.getId());
+                    }
+                }
+            }
+
             callbackContext.success(message);
 
             return true;
